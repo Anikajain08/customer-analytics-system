0733eec9d0f7309b23ea5755f19de11c84ba45fb
DEmo-1 <anikajain0810@gmail.com>
2026-05-24 19:37:47 +0530

    fixed churn deployment

--- src/churn_prediction.py
+++ src/churn_prediction.py
@@ -1,13 +1,4 @@
-import os
-import joblib
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-churn_model = joblib.load(os.path.join(BASE_DIR, 'models', 'churn_model.pkl'))
-
-import pandas as pd
-
 def predict_churn(rfm):
-    # define churn based on recency
+    # simple rule-based churn (no model needed)
     rfm['Churn'] = rfm['Recency'].apply(lambda x: 1 if x > 90 else 0)
     return rfm
