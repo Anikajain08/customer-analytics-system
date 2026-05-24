a06b497c7ac3de85a3f56822b5d2b123edf4d14f
DEmo-1 <anikajain0810@gmail.com>
2026-05-24 19:35:11 +0530

    removed churn model dependency

--- src/churn_prediction.py
+++ src/churn_prediction.py
@@ -5,10 +5,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 churn_model = joblib.load(os.path.join(BASE_DIR, 'models', 'churn_model.pkl'))
 
-# load model
-churn_model = joblib.load('models/churn_model.pkl')
+import pandas as pd
 
 def predict_churn(rfm):
-    X = rfm[['Recency', 'Frequency', 'Monetary']]
-    rfm['Churn'] = churn_model.predict(X)
+    # define churn based on recency
+    rfm['Churn'] = rfm['Recency'].apply(lambda x: 1 if x > 90 else 0)
     return rfm
